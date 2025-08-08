16b697791bfc5e181988829913006706ce8192cd
azapg <allangameyt28@gmail.com>
2025-08-07 22:28:46 -0500

    what

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Writer } from "../components/Writer";
+import { Writer } from "@/components/Writer";
 
 const WRITER_MAX_W = "max-w-5xl";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full flex justify-center ">
-      <div className={`pt-10 w-full ${WRITER_MAX_W}`}>
-        <Writer fontSize="clamp(28px,5vw,48px)" />
-      </div>
+      <Writer fontSize="clamp(28px,5vw,48px)" className={`pt-10 w-full ${WRITER_MAX_W}`}/>
     </main>
   );
 }
